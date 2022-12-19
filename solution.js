f6cf119c642446377fd7b693f64053d92573477b
dipak307 <114248721+dipak307@users.noreply.github.com>
2022-12-19 21:22:37 +0530

    Update solution.js

--- solution.js
+++ solution.js
@@ -2,5 +2,18 @@
 
 function palindrome(str){
 
+    s = s.replace(/[^0-9a-zA-Z]+/gmi,"");
+    s = s.toLowerCase();
+    var l = 0, r = s.length - 1;
+    console.log(s);
+
+    while(l<r) {
+        if(s.charAt(l) != s.charAt(r)) {
+            return false;
+        }
+        l++;
+        r--;
+    }
+    return true;
 }
 module.exports = palindrome
